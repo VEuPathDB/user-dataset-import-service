fc6e29dce53d623fb77209d0f583b2367f479981
Elizabeth Harper <foxcapade@gmail.com>
2020-06-02 18:08:04 -0400

    fixed arrayaggs

--- src/main/resources/sql/select/jobs-by-user.sql
+++ src/main/resources/sql/select/jobs-by-user.sql
@@ -1,10 +1,10 @@
 SELECT
   *,
-  array_agg((
-    SELECT project_id
+  (
+    SELECT array_agg(project_id)
     FROM ds_jobs.job_projects
     WHERE db_id = jobs.db_id
-  )) AS projects
+  ) AS projects
 FROM
   ds_jobs.jobs
   LEFT JOIN ds_jobs.job_messages
