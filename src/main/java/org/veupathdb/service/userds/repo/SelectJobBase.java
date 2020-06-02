f1c54f250cfde169de33e973185d30a59dc5e2c9
Elizabeth Harper <foxcapade@gmail.com>
2020-06-02 18:19:31 -0400

    OffsetDateTime

--- src/main/java/org/veupathdb/service/userds/repo/SelectJobBase.java
+++ src/main/java/org/veupathdb/service/userds/repo/SelectJobBase.java
@@ -3,6 +3,7 @@ package org.veupathdb.service.userds.repo;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
 
 import org.veupathdb.service.userds.model.JobRow;
@@ -29,8 +30,10 @@ abstract class SelectJobBase
       rs.getString(Schema.Column.NAME),
       rs.getString(Schema.Column.DESCRIPTION),
       rs.getString(Schema.Column.SUMMARY),
-      rs.getObject(Schema.Column.STARTED, LocalDateTime.class),
-      rs.getObject(Schema.Column.FINISHED, LocalDateTime.class),
+      rs.getObject(Schema.Column.STARTED, OffsetDateTime.class)
+        .toLocalDateTime(),
+      rs.getObject(Schema.Column.FINISHED, OffsetDateTime.class)
+        .toLocalDateTime(),
       rs.getString(Schema.Column.MESSAGE),
       projects
     );
