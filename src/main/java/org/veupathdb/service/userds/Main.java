ede4d9da68c545d8012df1f3f5d46c60c6d10753
Elizabeth Harper <foxcapade@gmail.com>
2020-06-04 13:08:09 -0400

    disabled jersey tracing

--- src/main/java/org/veupathdb/service/userds/Main.java
+++ src/main/java/org/veupathdb/service/userds/Main.java
@@ -55,14 +55,7 @@ public class Main extends Server {
 
   @Override
   protected ContainerResources newResourceConfig(Options options) {
-    final var out = new Resources(options);
-
-    // Enabled by default for debugging purposes, this should be removed when
-    // production ready.
-    out.property("jersey.config.server.tracing.type", "ALL")
-      .property("jersey.config.server.tracing.threshold", "VERBOSE");
-
-    return out;
+    return new Resources(options);
   }
 
   @Override
